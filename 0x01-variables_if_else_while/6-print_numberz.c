8a07f4c36ec875ea3121b2ec98f518ca9800b93a
sunnec <you@example.com>
2023-02-19 08:33:35 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Program that prints all single digit numbers of base 10
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	a = 0;
+	while
+		(a < 10) {
+			putchar(a + '0');
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
