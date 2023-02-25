7db38ad0f2628ae4a82c59731da5ba2dd0551ef2
sunnec <you@example.com>
2023-02-24 20:49:14 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,19 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+*print_most_numbers - prints
+* owned by Bwave ICT
+*Return: void
+*/
+
+void print_most_numbers(void)
 {
-	print_most_numbers();
-	return (0);
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+	if (!(c == '2' || c == '4'))
+	_putchar(c);
+	}
+	_putchar('\n');
 }
