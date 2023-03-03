f84ed145f39acb1354317470595f244be2ae03c3
sunnec <you@example.com>
2023-03-02 21:08:42 -0800

    103-infinite_add.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  * print_number - function that prints any integer with putchar
+  *
+  * @n: number to be printed
+  *
+  * Return: void
+  */
+
+void print_number(int n)
+{
+	unsigned int i;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	i = n;
+	if (i / 10)
+		print_number(i / 10);
+
+	_putchar(i % 10 + '0');
+}
